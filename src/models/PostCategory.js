699ea264c88b9d2bf4b170f621820dd3624aed72
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-02-01 07:05:52 -0300

    Criado model postCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    'PostCategory',
+    {
+      postId: { type: DataTypes.INTEGER, foreignKey: true, primaryKey: true },
+      categoryId: { type: DataTypes.INTEGER, foreignKey: true, primaryKey: true },
+    },
+    {
+      tableName: 'posts_categories',
+      timestamps: false,
+      underscored: true,
+    }
+  );
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  }
+  return PostCategory;
+};
