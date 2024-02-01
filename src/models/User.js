9d47e97ffa77278f07b83591c7c3c976318d9e82
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-02-01 01:54:46 -0300

    Criado model para user

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      tableName: 'users',
+      timestamps: false,
+      underscored: true,
+    }
+  );
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'blogPosts' })
+  }
+  return User;
+};
